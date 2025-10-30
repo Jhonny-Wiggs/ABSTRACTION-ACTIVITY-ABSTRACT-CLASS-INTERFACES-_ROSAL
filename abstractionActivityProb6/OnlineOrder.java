13f3c1665d173c29a524d883279f8064044a43f9
Adrian Rosal <snowyangel543@gmail.com>
2025-10-30 19:07:37 +0800

    Add @Override annotation to placeOrder method

--- abstractionActivityProb6/OnlineOrder.java
+++ abstractionActivityProb6/OnlineOrder.java
@@ -1,7 +1,9 @@
 package abstractionActivityProb6;
 
 public class OnlineOrder implements Order{
+	@Override
 	public void placeOrder(String itemName) {
         System.out.println("Online order placed for: " + itemName);
     }
 }
+
