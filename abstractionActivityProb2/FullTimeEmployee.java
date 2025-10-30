4b5666b6c1d15ec33cc326feef80cf00fd43c186
Adrian Rosal <snowyangel543@gmail.com>
2025-10-30 13:26:31 +0800

    Update FullTimeEmployee.java

--- abstractionActivityProb2/FullTimeEmployee.java
+++ abstractionActivityProb2/FullTimeEmployee.java
@@ -4,8 +4,10 @@ public class FullTimeEmployee extends Employee{
 	FullTimeEmployee(String name, double ratePerHour, int hoursWorked) {
         super(name, ratePerHour, hoursWorked);
     }
-
+	
+	@Override
     double calculatePay() {
         return ratePerHour * hoursWorked;
     }
 }
+
