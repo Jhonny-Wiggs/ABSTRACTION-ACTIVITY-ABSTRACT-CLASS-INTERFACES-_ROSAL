1fbcd935f1fc7249a75484d241be6d9cd883fc93
Adrian Rosal <snowyangel543@gmail.com>
2025-10-30 19:00:32 +0800

    Add @Override annotations to Car methods

--- abstractionActivityProb3/Car.java
+++ abstractionActivityProb3/Car.java
@@ -3,12 +3,15 @@ package abstractionActivityProb3;
 public class Car implements Vehicle{
 	int speed = 0;
 
+	@Override
     public void start() {
         System.out.println("Car started.");
     }
-
+	
+	@Override
     public void speedUp(int increment) {
         speed += increment;
         System.out.println("Car speed: " + speed + " km/h");
     }
 }
+
