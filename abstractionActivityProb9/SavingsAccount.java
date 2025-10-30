0c9af5f836ce4d92b0322b2f2cdca39408229e96
Adrian Rosal <snowyangel543@gmail.com>
2025-10-30 19:09:59 +0800

    Update SavingsAccount.java

--- abstractionActivityProb9/SavingsAccount.java
+++ abstractionActivityProb9/SavingsAccount.java
@@ -2,12 +2,12 @@ package abstractionActivityProb9;
 
 public class SavingsAccount implements BankAccount{
 	double balance = 0;
-
+	@Override
     public void deposit(double amount) {
         balance += amount;
         System.out.printf("Deposited: %.2f, Balance: %.2f%n", amount, balance);
     }
-
+	@Override
     public void withdraw(double amount) {
         if (amount <= balance) {
             balance -= amount;
@@ -17,3 +17,4 @@ public class SavingsAccount implements BankAccount{
         }
     }
 }
+
