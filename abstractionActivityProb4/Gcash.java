1fb97d1dce6e121c5317617043eee9bd63c28eaf
Adrian Rosal <snowyangel543@gmail.com>
2025-10-30 19:07:04 +0800

    Update Gcash.java

--- abstractionActivityProb4/Gcash.java
+++ abstractionActivityProb4/Gcash.java
@@ -1,7 +1,9 @@
 package abstractionActivityProb4;
 
 public class Gcash implements Payment{
+	@Override
 	public void processPayment(double amount) {
         System.out.println("Paid " + amount + " using Gcash.");
     }
 }
+
