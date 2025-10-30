7a5a3bca09f723f4f8748cc21151910183cde1d6
Adrian Rosal <snowyangel543@gmail.com>
2025-10-30 19:08:52 +0800

    Add @Override annotation to print method

--- abstractionActivityProb8/ImagePrinter.java
+++ abstractionActivityProb8/ImagePrinter.java
@@ -1,7 +1,9 @@
 package abstractionActivityProb8;
 
 public class ImagePrinter implements Printable{
+	@Override
 	public void print(String content) {
 	    System.out.println("Printing image: " + content);
 	}
 }
+
