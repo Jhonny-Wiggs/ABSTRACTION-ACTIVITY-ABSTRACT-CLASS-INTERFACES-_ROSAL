223415f6eb627cd90522503c82d077d33ea4da95
Adrian Rosal <snowyangel543@gmail.com>
2025-10-30 19:07:17 +0800

    Update CreditCard.java

--- abstractionActivityProb4/CreditCard.java
+++ abstractionActivityProb4/CreditCard.java
@@ -1,7 +1,9 @@
 package abstractionActivityProb4;
 
 public class CreditCard implements Payment{
+	@Override
 	public void processPayment(double amount) {
         System.out.println("Paid " + amount + " using Credit Card.");
     }
 }
+
